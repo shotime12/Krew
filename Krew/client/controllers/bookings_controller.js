44014e4f304e3343c64588dea63cc6b173f19563
Shovit Kandel <shovit.kandel@gmail.com>
2017-05-02 18:30:44 -0500

    adding autocomplete

--- Krew/client/controllers/bookings_controller.js
+++ Krew/client/controllers/bookings_controller.js
@@ -1,13 +1,35 @@
+app.directive('googleplace', function(){
+	 return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, model) {
+            var options = {
+                types: [],
+                componentRestrictions: {country: 'US'}
+            };
+            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                scope.$apply(function() {
+                    model.$setViewValue(element.val());                
+                });
+            });
+        }
+    };
+})
+
 app.controller('booking_controller', function($scope, $location, BookingFactory){
 
+	$scope.gPlace;
 	//Creating toggle for form
 	$scope.show = 0;
+
+	$scope.date = new Date();
 
 	function setBookings(data){
 		$scope.confirms = data;
 		$scope.newBooking = {};
 	}
-	
+
 	$scope.bookings = [];
 
 	//fetch the bookings when controller loaded
